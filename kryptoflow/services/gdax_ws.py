cfbd6a6dde3d68247146fb8731e88e4508ed1334
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2018-03-11 20:18:44 -0400

    new service based on kafka direct streaming

---
+++ kryptoflow/services/gdax_ws.py
@@ -0,0 +1,62 @@
+import gdax
+from datetime import datetime
+from kryptoflow.models.streamer_base import AvroAsync
+
+
+class GDAXClient(gdax.WebsocketClient):
+
+    def __init__(self, products=None, channels=None, producer=None):
+
+        super(GDAXClient, self).__init__(products=products, channels=channels)
+        self.topic = 'gdax'
+        self.url = "wss://ws-feed.gdax.com/ticker"
+        self.producer = producer
+
+    def on_message(self, message):
+        if self.validate_msg(message):
+            return
+        else:
+            message = self._format_message(message)
+            self.producer.produce(topic=self.topic, value=message)
+            self.producer.flush()
+
+    def start_stream(self):
+        try:
+            self.start()
+        except Exception as e:
+            print('ERRROR', e)
+            self.start_stream()
+
+    @staticmethod
+    def validate_msg(msg):
+        if any([i not in msg.keys() for i in ['price', 'time', 'best_ask', 'best_bid']]):
+            return True
+        else:
+            return False
+
+    def on_open(self):
+        self.url = "wss://ws-feed.gdax.com/ticker"
+        self.products = ["BTC-USD"]
+
+    @staticmethod
+    def calculate_spread(msg):
+        return abs(float(msg['best_bid']) - float(msg['best_ask']))
+
+    def _format_message(self, message):
+        msg = {'price': float(message['price']),
+               'ts': str(datetime.strptime(message['time'], "%Y-%m-%dT%H:%M:%S.%fZ")),
+               'volume_24h': float(message['volume_24h']),
+               'spread': self.calculate_spread(message),
+               'side': message['side']}
+
+        return msg
+
+    def on_close(self):
+        print("-- Goodbye! --")
+
+
+if __name__ == '__main__':
+
+    sink = AvroAsync(topic='gdax')
+    gd = GDAXClient(products=['BTC-USD'], channels=['ticker'], producer=sink.producer())
+    gd.start_stream()
