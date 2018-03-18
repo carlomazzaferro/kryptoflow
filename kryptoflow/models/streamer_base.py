e1ff190d4f7ca84f89a944e3f59ee7c750c7576c
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2018-03-18 19:58:40 -0400

    reading log from remote server

--- kryptoflow/models/streamer_base.py
+++ kryptoflow/models/streamer_base.py
@@ -2,60 +2,66 @@ import abc
 from kryptoflow.services.utilities.utils import TimeUtils, RepeatedTimer
 from kryptoflow.definitions import SCHEMAS
 import os
-from confluent_kafka import avro
-from confluent_kafka.avro import AvroProducer
-from collections import defaultdict
+from confluent_kafka import avro, TopicPartition
+from confluent_kafka.avro import AvroProducer, AvroConsumer
+from confluent_kafka import KafkaError
+import confluent_kafka
 
 
-class Streamer(abc.ABC):
+class AvroAsync(object):
 
     def __init__(self, topic=None):
-        super(Streamer, self).__init__()
-
-        self.key_schema = avro.load(os.path.join(SCHEMAS, 'keyschema.avsc'))
         self.topic = topic
-        self.producer = AvroProducer({'bootstrap.servers': '127.0.0.1:9092',
-                                      'schema.registry.url': 'http://127.0.0.1:8081'},
-                                     default_key_schema=self.key_schema,
-                                     default_value_schema=avro.load(os.path.join(SCHEMAS, self.topic + '.avsc')))
+        self.ip = os.environ['KAFKA_SERVER_IP']
+        self.base_config = {'bootstrap.servers': self.ip + ':9092',
+                            'schema.registry.url': 'http://' + self.ip + ':8081'}
 
-        print(os.path.join(SCHEMAS, self.topic + '.avsc'))
-        self.timer = RepeatedTimer
-        self.time_util = TimeUtils()
+        self.avro_consumer = AvroConsumer(dict(self.base_config, **{'group.id': 'groupid'}))
 
-    @abc.abstractmethod
-    def cache(self):
-        return
+        self.avro_consumer.assign([TopicPartition(self.topic, 0)])
+        self.key_schema = avro.load(os.path.join(SCHEMAS, 'keyschema.avsc'))
+        self.value_schema = avro.load(os.path.join(SCHEMAS, self.topic + '.avsc'))
 
-    @abc.abstractmethod
-    def as_producer(self):
-        return
+    def producer(self):
+        return AvroProducer({'bootstrap.servers': 'localhost:9092',
+                             'schema.registry.url': 'http://localhost:8081'},
+                            default_key_schema=self.key_schema,
+                            default_value_schema=self.value_schema)
 
-    def send(self, message):
-        message['ts'] = str(message['ts'])
-        self.producer.produce(topic=self.topic, value=message)  #, key=message.keys())
-        self.producer.flush()
+    def read_new(self):
 
-    def _release_cache(self):
-        self._accumulator = defaultdict(list)
-        self._comment_accumulator = self._tweet_accumulator = {'sentences': [],
-                                                               'sentence_count': 0,
-                                                               'polarity': 0}
+        self.avro_consumer.subscribe(['reddit'])
+
+        running = True
+
+        while running:
+            msg = self.avro_consumer.poll()
+            if not msg.error():
+                print(msg.value())
+            elif msg.error().code() != KafkaError._PARTITION_EOF:
+                print(msg.error())
+                running = False
+        self.avro_consumer.close()
+
+    def read_from_start(self):
+        c = AvroConsumer(dict(self.base_config, **{'group.id': 'groupid',
+                                                   'default.topic.config': {'auto.offset.reset': 'beginning',
+                                                                            'auto.commit.enable': 'false'}
+                                                   }
+                              )
+                         )
+
+        c.assign([TopicPartition('reddit', partition=0, offset=confluent_kafka.OFFSET_BEGINNING)])
+
+        counter = 0
+        while True:
+            counter += 1
+            msg = c.poll()
+            print(msg.value())
+            print(a)
 
 
-class MergedStream(Streamer):
+if __name__ == '__main__':
 
-    def __init__(self, stream_list):
-        super().__init__()
-
-        self.stream_list = stream_list
-
-    def stream(self):
-        return
-
-    def cache(self):
-        return
-
-    def as_producer(self):
-        return
-
+    a = AvroAsync(topic='reddit')
+    a.read_from_start()
