cd1b8d7076528fe2b8938aa344f5ab22f582d358
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2018-03-26 22:32:29 -0400

    docker file and executable to build serve-able tf model

---
+++ kryptoflow/serving/serve.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker cp stored_models/$1/tf tf_serving:/serving
+docker start tf_serving && docker exec -it tf_serving mv /serving/tf /serving/$1
+docker exec -it tf_serving /serving/bazel-bin/tensorflow_serving/model_servers/tensorflow_model_server \
+    --port=9000 --model_name=base --model_base_path=/serving/$1 &> gan_log &
