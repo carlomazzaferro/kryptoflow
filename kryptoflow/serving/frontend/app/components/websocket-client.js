15231ca68fe8c57ced2221d8c9e6edc25663f3fb
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2018-05-11 02:29:42 -0400

    live charts functional;ity with websockets

---
+++ kryptoflow/serving/frontend/app/components/websocket-client.js
@@ -0,0 +1,10 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+
+
+  actions: {
+
+  }
+});
--- kryptoflow/serving/frontend/config/environment.js
+++ kryptoflow/serving/frontend/config/environment.js
@@ -2,6 +2,9 @@
 
 module.exports = function(environment) {
   let ENV = {
+    'ember-websockets': {
+      socketIO: true
+    },
     modulePrefix: 'frontend',
     environment,
     rootURL: '/',
@@ -21,7 +24,29 @@ module.exports = function(environment) {
       // Here you can pass flags/options to your application instance
       // when it is created
     }
+    // contentSecurityPolicy: {
+    //   'default-src': "'none'",
+    //   'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
+    //   'font-src': "'self'",
+    //   'connect-src': "'self' ws://0.0.0.0:5000 0.0.0.0:5000",
+    //   'img-src': "'self'",
+    //   'report-uri':"'0.0.0.0'",
+    //   'style-src': "'self' 'unsafe-inline'",
+    //   'frame-src': "'none'"
+    // }
   };
+
+  ENV['x-toggle'] = {
+    includedThemes: ['light', 'default', 'flip'],
+    excludedThemes: ['flip'],
+    excludeBaseStyles: false, // defaults to false
+    defaultShowLabels: true,  // defaults to false
+    defaultTheme: 'light',    // defaults to 'default'
+    defaultSize: 'small',     // defaults to 'medium'
+    defaultOffLabel: 'False', // defaults to 'Off'
+    defaultOnLabel: 'True'    // defaults to 'On'
+  };
+
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
@@ -49,3 +74,5 @@ module.exports = function(environment) {
 
   return ENV;
 };
+
+
