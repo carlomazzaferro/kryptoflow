63acf86ae9e63a31d7d63c9f2fbe26792c7e0840
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2018-06-14 20:09:22 -0400

    removed serving module, as it was moved to its own repo

--- kryptoflow/serving/resources/build_server.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --pull -t $USER/tensorflow-serving-devel -f kryptoflow/serving/Dockerfile .
-docker run -it $USER/tensorflow-serving-devel --name tf_serving
--- kryptoflow/serving/resources/serve.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker cp stored_models/$1/tf tf_serving:/serving
-docker start tf_serving && docker exec -it tf_serving mv /serving/tf /serving/$1
-docker exec -it tf_serving /serving/bazel-bin/tensorflow_serving/model_servers/tensorflow_model_server \
-    --port=9000 --model_base_path=/serving/ &> gan_log &
