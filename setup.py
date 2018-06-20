2a83eb8db483c1e562c7dc000f508056e48ada91
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2018-06-19 21:33:33 -0400

    Travis CI with tox

--- setup.py
+++ setup.py
@@ -3,19 +3,28 @@
 """
     Setup file for kryptoflow.
 
-    This file was generated with PyScaffold 2.5.8, a tool that easily
-    puts up a scaffold for your new Python project. Learn more under:
-    http://pyscaffold.readthedocs.org/
+    This file was generated with PyScaffold 3.0.3.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: http://pyscaffold.org/
 """
 
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
+
+# Add here console scripts and other entry points in ini-style format
+entry_points = """
+[console_scripts]
+# script_name = kafka_tfrx.module:function
+# For example:
+# fibonacci = kafka_tfrx.skeleton:run
+"""
 
 
 def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx'] if needs_sphinx else []
-    setup(use_pyscaffold=True)
+    setup(entry_points=entry_points,
+          version='0.12',
+          tests_require=['pytest', 'pytest-cov', 'pytest-runner'],
+          packages=find_packages(exclude=['docs', 'tests'], include=['kryptoflow']))
 
 
 if __name__ == "__main__":
