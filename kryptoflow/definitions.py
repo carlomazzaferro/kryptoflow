2d0a7ac7daf71fc2ce259c18015579705805a4e3
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2018-06-26 23:25:00 -0400

    more refactoring to accomodate for new cli design

--- kryptoflow/definitions.py
+++ kryptoflow/definitions.py
@@ -4,6 +4,9 @@ ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 CONFIG_PATH = os.path.join(ROOT_DIR, 'resources/resources.json')
 RESOURCES_PATH = os.path.join(ROOT_DIR, 'resources')
 SCHEMAS = os.path.join(ROOT_DIR, 'resources/schemas')
-SAVED_MODELS = os.path.join(ROOT_DIR, 'stored_models')
 APP_DIR = os.path.join(os.path.join(ROOT_DIR, 'kryptoflow/serving'))
 TIMEFRAME = 8
+model_dir = os.path.join(os.environ['HOME'], 'SAVED_MODELS')
+if not os.path.exists(model_dir):
+    os.mkdir(model_dir)
+    SAVED_MODELS = model_dir
