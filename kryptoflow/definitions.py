6f981be70efa9e072af3ca71494280d1babb606a
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2018-09-30 23:27:50 -0400

    removing unneeded file

---
+++ kryptoflow/definitions.py
@@ -0,0 +1,10 @@
+import os
+
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+CONFIG_PATH = os.path.join(ROOT_DIR, 'resources/resources.json')
+RESOURCES_PATH = os.path.join(ROOT_DIR, 'resources')
+SCHEMAS = os.path.join(ROOT_DIR, 'resources/schemas')
+APP_DIR = os.path.join(os.path.join(ROOT_DIR, 'kryptoflow/serving'))
+TIMEFRAME = 8
+
+
